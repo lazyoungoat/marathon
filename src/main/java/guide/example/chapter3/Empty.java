ce60bf2a8a77c6959b2145f755a7737ac3a5cf3e
Nikita <l.r.shiun@gmail.com>
2022-04-07 12:25:18 +0800

    continue

---
+++ src/main/java/guide/example/chapter3/Empty.java
@@ -0,0 +1,15 @@
+package guide.example.chapter3;
+
+//Parts of the for can be empty
+public class Empty {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i < 10; ) {
+            System.out.println("Pass #" + i);
+            i++; // increment loop control var
+
+        }
+
+    }
+}
---
+++ src/main/java/guide/example/chapter3/Empty2.java
@@ -0,0 +1,12 @@
+package guide.example.chapter3;
+
+public class Empty2 {
+    public static void main(String[] args) {
+        int i = 0; // move initialization out of loop
+        for (; i < 10; ){
+            System.out.println("Pass #" + i);
+            i++; // increment loop control var
+
+        }
+    }
+}
