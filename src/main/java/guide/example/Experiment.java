62ecb501de38ff40cb1c564d943047bcd10738a2
Nikita <l.r.shiun@gmail.com>
2022-04-12 11:30:43 +0800

    practice 99

--- src/main/java/guide/example/Experiment.java
+++ src/main/java/guide/example/Experiment.java
@@ -1,9 +1,26 @@
 package guide.example;
 
+import java.util.Scanner;
+
 public class Experiment {
     public static void main(String[] args)
     throws  java.io.IOException{
 
+        System.out.println("Please enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.next();
+        int num = Integer.parseInt(string);
+
+        insert:
+            for (int i = num; i < 10; i++) {
+                for (int j = 1; j < 10; j++) {
+                    if (num<1)
+                        break insert;
+
+                    System.out.print(i + "*" + j +"=" + i*j+"\t");
+                }
+                System.out.println();
+            }
 
     }
 
